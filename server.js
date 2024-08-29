5fca0486d043a21a7f49b15cdda89d504a15732a
mohitbhagi <68736192+mohitbhagi@users.noreply.github.com>
2024-08-29 19:38:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const makeSpellCheckRequest = require('./spellcheck');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
